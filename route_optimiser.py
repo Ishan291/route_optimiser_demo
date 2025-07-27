5ce1addba670d98d6f5d3bfeb7f18ce3508d4abb
Ishan291 <ishantechtrio@gmail.com>
2025-07-28 03:11:48 +0530

    Update route_optimiser.py

--- route_optimiser.py
+++ route_optimiser.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/19W5bN73mFsBCZseZS6s3XHBIfH7Hn0X_
 """
 
-!pip install streamlit
 import streamlit as st
 import numpy as np
 import random
